5e8f8a53777f6936571785351e54cd45ecab79ea
RehanaMittamidi <mittamidirehana2005@gmail.com>
2025-06-01 17:40:06 +0530

    Create corejava.java

---
+++ corejava.java
@@ -0,0 +1,222 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld stub executed.");
+    }
+}
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        System.out.println("SimpleCalculator stub executed.");
+    }
+}
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        System.out.println("EvenOddChecker stub executed.");
+    }
+}
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        System.out.println("LeapYearChecker stub executed.");
+    }
+}
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("MultiplicationTable stub executed.");
+    }
+}
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        System.out.println("DataTypeDemo stub executed.");
+    }
+}
+public class TypeCasting {
+    public static void main(String[] args) {
+        System.out.println("TypeCasting stub executed.");
+    }
+}
+public class OperatorPrecedence {
+    public static void main(String[] args) {
+        System.out.println("OperatorPrecedence stub executed.");
+    }
+}
+public class GradeCalculator {
+    public static void main(String[] args) {
+        System.out.println("GradeCalculator stub executed.");
+    }
+}
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        System.out.println("NumberGuessingGame stub executed.");
+    }
+}
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        System.out.println("FactorialCalculator stub executed.");
+    }
+}
+public class MethodOverloading {
+    public static void main(String[] args) {
+        System.out.println("MethodOverloading stub executed.");
+    }
+}
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        System.out.println("RecursiveFibonacci stub executed.");
+    }
+}
+public class ArraySumAverage {
+    public static void main(String[] args) {
+        System.out.println("ArraySumAverage stub executed.");
+    }
+}
+public class StringReversal {
+    public static void main(String[] args) {
+        System.out.println("StringReversal stub executed.");
+    }
+}
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        System.out.println("PalindromeChecker stub executed.");
+    }
+}
+public class CarClass {
+    public static void main(String[] args) {
+        System.out.println("CarClass stub executed.");
+    }
+}
+public class InheritanceExample {
+    public static void main(String[] args) {
+        System.out.println("InheritanceExample stub executed.");
+    }
+}
+public class InterfaceImplementation {
+    public static void main(String[] args) {
+        System.out.println("InterfaceImplementation stub executed.");
+    }
+}
+public class TryCatchExample {
+    public static void main(String[] args) {
+        System.out.println("TryCatchExample stub executed.");
+    }
+}
+public class CustomException {
+    public static void main(String[] args) {
+        System.out.println("CustomException stub executed.");
+    }
+}
+public class FileWriting {
+    public static void main(String[] args) {
+        System.out.println("FileWriting stub executed.");
+    }
+}
+public class FileReading {
+    public static void main(String[] args) {
+        System.out.println("FileReading stub executed.");
+    }
+}
+public class ArrayListExample {
+    public static void main(String[] args) {
+        System.out.println("ArrayListExample stub executed.");
+    }
+}
+// HashMapExample.java
+public class HashMapExample {
+    public static void main(String[] args) {
+        System.out.println("HashMapExample stub executed.");
+    }
+}
+// ThreadExample.java
+public class ThreadExample {
+    public static void main(String[] args) {
+        System.out.println("ThreadExample stub executed.");
+    }
+}
+// LambdaSort.java
+public class LambdaSort {
+    public static void main(String[] args) {
+        System.out.println("LambdaSort stub executed.");
+    }
+}
+// StreamEvenFilter.java
+public class StreamEvenFilter {
+    public static void main(String[] args) {
+        System.out.println("StreamEvenFilter stub executed.");
+    }
+}
+// RecordExample.java
+public class RecordExample {
+    public static void main(String[] args) {
+        System.out.println("RecordExample stub executed.");
+    }
+}
+// PatternMatchingSwitch.java
+public class PatternMatchingSwitch {
+    public static void main(String[] args) {
+        System.out.println("PatternMatchingSwitch stub executed.");
+    }
+}
+// JDBCConnection.java
+public class JDBCConnection {
+    public static void main(String[] args) {
+        System.out.println("JDBCConnection stub executed.");
+    }
+}
+// JDBCInsertUpdate.java
+public class JDBCInsertUpdate {
+    public static void main(String[] args) {
+        System.out.println("JDBCInsertUpdate stub executed.");
+    }
+}
+// JDBCTransaction.java
+public class JDBCTransaction {
+    public static void main(String[] args) {
+        System.out.println("JDBCTransaction stub executed.");
+    }
+}
+// JavaModules.java
+public class JavaModules {
+    public static void main(String[] args) {
+        System.out.println("JavaModules stub executed.");
+    }
+}
+// TCPChat.java
+public class TCPChat {
+    public static void main(String[] args) {
+        System.out.println("TCPChat stub executed.");
+    }
+}
+// HTTPClient.java
+public class HTTPClient {
+    public static void main(String[] args) {
+        System.out.println("HTTPClient stub executed.");
+    }
+}
+// BytecodeInspect.java
+public class BytecodeInspect {
+    public static void main(String[] args) {
+        System.out.println("BytecodeInspect stub executed.");
+    }
+}
+// DecompileClass.java
+public class DecompileClass {
+    public static void main(String[] args) {
+        System.out.println("DecompileClass stub executed.");
+    }
+}
+// ReflectionExample.java
+public class ReflectionExample {
+    public static void main(String[] args) {
+        System.out.println("ReflectionExample stub executed.");
+    }
+}
+// VirtualThreads.java
+public class VirtualThreads {
+    public static void main(String[] args) {
+        System.out.println("VirtualThreads stub executed.");
+    }
+}
+// ExecutorCallable.java
+public class ExecutorCallable {
+    public static void main(String[] args) {
+        System.out.println("ExecutorCallable stub executed.");
+    }
+}
